5c9c3f9426d7713a6870c2721d099a2d098b1c53
skander3310 <54539432+skander3310@users.noreply.github.com>
2019-08-30 14:48:46 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function bold(){
+    document.getElementById("textarea").style.fontWeight="bold";
+}
+
+function italic(){
+    document.getElementById("textarea").style.fontStyle="italic"
+}
+function underline(){
+    document.getElementById("textarea").style.textDecoration="underline"
+}
+function huhu(){
+    document.getElementById("textarea").style.fontSize=document.getElementById("taille").value
+}
+function hoho(){
+  document.getElementById ("textarea").style.fontFamily=document.getElementById("family").value
+}
+
+
